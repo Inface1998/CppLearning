f7adda03b463ec618536da13810b79c29c334bd2
Robert <1105417715@qq.com>
2022-11-24 15:26:51 +0800

    11/24

--- bookcodes/chapter04/address.cpp
+++ bookcodes/chapter04/address.cpp
@@ -1,11 +1,11 @@
 // address.cpp -- using the & operator to find addresses
 #include <iostream>
+using namespace std;
 int main()
 {
-    using namespace std;
+
     int donuts = 6;
     double cups = 4.5;
-
     cout << "donuts value = " << donuts;
     cout << " and donuts address = " << &donuts << endl;
 // NOTE: you may need to use unsigned (&donuts)
